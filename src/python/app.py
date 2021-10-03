f87f48645c6092c203ccce5dc215ce8cdba0074a
j0z07b8 <jiaen.zhang@samsclub.com>
2021-10-03 03:40:23 -0500

    basic flask app

--- src/python/app.py
+++ src/python/app.py
@@ -1,1 +1,16 @@
-from matplotlib import pyplot, animation
+import flask
+from src.python import tle
+
+app = flask.Flask(__name__)
+
+payload = {"key": "value"}
+
+
+@app.route('/payload', methods=['GET'])
+def getPayload():
+    return flask.jsonify(tle.loadTLE())
+
+
+if __name__ == '__main__':
+    print("logging: Running on http://127.0.0.1:5000/payload")
+    app.run(debug=True)
