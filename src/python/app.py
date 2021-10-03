f78906d39778a29921b879fb93c1a5e048090805
j0z07b8 <jiaen.zhang@samsclub.com>
2021-10-03 14:24:30 -0500

    added backend geocoding function and basic front-end

--- src/python/app.py
+++ src/python/app.py
@@ -1,5 +1,5 @@
 import flask
-from src.python import tle
+from src.python import tle, geocoding
 
 app = flask.Flask(__name__)
 
@@ -9,6 +9,12 @@ def getPayload():
     return flask.jsonify(tle.loadTLE())
 
 
+@app.route('/location', methods=['GET'])
+def getLatLong():
+    return flask.jsonify(geocoding.getLatLong()[1])
+
+
 if __name__ == '__main__':
     print("logging: Running on http://127.0.0.1:5000/tle")
+    print("logging: Running on http://127.0.0.1:5000/location")
     app.run(debug=True)
