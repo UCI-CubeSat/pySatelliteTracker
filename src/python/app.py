74db17b67a8ba3e28a35ec374ce7b1e3561f7214
j0z07b8 <jiaen.zhang@samsclub.com>
2021-10-03 03:42:37 -0500

    basic flask app

--- src/python/app.py
+++ src/python/app.py
@@ -3,14 +3,12 @@ from src.python import tle
 
 app = flask.Flask(__name__)
 
-payload = {"key": "value"}
 
-
-@app.route('/payload', methods=['GET'])
+@app.route('/tle', methods=['GET'])
 def getPayload():
     return flask.jsonify(tle.loadTLE())
 
 
 if __name__ == '__main__':
-    print("logging: Running on http://127.0.0.1:5000/payload")
+    print("logging: Running on http://127.0.0.1:5000/tle")
     app.run(debug=True)
