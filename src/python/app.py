b5e46b085e19e5b293181cf6fa4865fbda87a36e
rubytsai <yijut2@uci.edu>
2021-10-27 02:06:04 -0700

    Added the index.html and modified the app.py to show the latlong result of Irvine. Able to handle the click on OSM to show latlong
    1. Simply use the render_template from Flask --> will later change to react or angular after I finish learning.
    2. Not implemented same function for all other cities yet

--- src/python/app.py
+++ src/python/app.py
@@ -1,4 +1,5 @@
 import flask
+from flask import render_template, request
 import requests
 from skyfield.toposlib import wgs84
 
@@ -19,7 +20,23 @@ def getPayload():
 
 @app.route('/location', methods=['GET'])
 def getLatLong():
+    print(geocoding.getLatLong()[0])
+    # print(test)
     return flask.jsonify(geocoding.getLatLong()[1])
+
+
+@app.route('/search')
+def search():
+    return render_template("index.html")
+
+@app.route('/view', methods=['GET', 'POST'])
+def viewLatLong():
+    output = request.form.to_dict()
+    print(output)
+    address = output["address"]
+    lat = geocoding.getLatLong()[0][0]
+    long = geocoding.getLatLong()[0][1]
+    return render_template("index.html", address = address, lat = lat, long = long)
 
 
 @app.route('/flight_path', methods=['GET'])
@@ -43,4 +60,5 @@ if __name__ == '__main__':
     print("logging: Running on http://127.0.0.1:5000/location")
     print("logging: Running on http://127.0.0.1:5000/flight_path")
     print("logging: Running on http://127.0.0.1:5000/flight_horizon")
+    print("logging: Running on http://127.0.0.1:5000/search")
     app.run(debug=True)
